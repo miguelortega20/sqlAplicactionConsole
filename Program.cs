d1c8ff360d3a00199f766b96f34f5721ae3aa8a8
Miguel Angel Ortega Flores <miguelortega.20@outlook.com>
2019-09-30 01:49:01 -0500

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sqlpractica3
+{
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
