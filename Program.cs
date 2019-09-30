101567643e583da993966e1dde23c5469eeeaebd
Omar426-boop <omartuzchan@gmail.com>
2019-09-30 13:59:34 -0500

    cambio3

--- Program.cs
+++ Program.cs
@@ -13,6 +13,19 @@ class Program
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            // UPDATE demo
+                    String userToUpdate = "Nikita";
+                    Console.Write("Updating 'Location' for user '" + userToUpdate + "', press any key to continue...");
+                    Console.ReadKey(true);
+                    sb.Clear();
+                    sb.Append("UPDATE Employees SET Location = N'United States' WHERE Name = @name");
+                    sql = sb.ToString();
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@name", userToUpdate);
+                        int rowsAffected = command.ExecuteNonQuery();
+                        Console.WriteLine(rowsAffected + " row(s) updated");
+                    }
         }
     }
 }
