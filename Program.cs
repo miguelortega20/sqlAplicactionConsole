87ac9eafd7799b94d3df13c65a3284bd27fd5a1d
luceg <lucerogonzalez942@gmail.com>
2019-09-30 06:36:07 -0500

    cambio1

--- Program.cs
+++ Program.cs
@@ -13,6 +13,24 @@ class Program
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+
+// INSERT demo
+                    Console.Write("Inserting a new row into table, press any key to continue...");
+                    Console.ReadKey(true);
+                    sb.Clear();
+                    sb.Append("INSERT Employees (Name, Location) ");
+                    sb.Append("VALUES (@name, @location);");
+                    sql = sb.ToString();
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@name", "Jake");
+                        command.Parameters.AddWithValue("@location", "United States");
+                        int rowsAffected = command.ExecuteNonQuery();
+                        Console.WriteLine(rowsAffected + " row(s) inserted");
+                    }
+
+
         }
     }
-}
+
